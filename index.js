97ae24da29ecd69adfc5eb7a44159431ee8a7c51
CarlosEduardo214 <cers.405@gmail.com>
2025-04-01 19:08:49 -0300

    terceiro desafio criado

---
+++ index.js
@@ -0,0 +1,30 @@
+class heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo.toLowerCase();
+    }
+
+    atacar() {
+        const ataques = {
+            mago: "magia",
+            guerreiro: "espada",
+            monge: "artes marciais",
+            ninja: "shuriken",
+        };
+
+        if (ataques[this.tipo]) {
+            console.log(`${this.nome}, o ${this.tipo}, atacou usando ${ataques[this.tipo]}.`);
+        }
+        else {
+            console.log(`${this.nome}, tentou atacar,mas o tipo de ataque é desconhecido.`);
+        }
+    }    
+}    
+
+//Criando instâncias e chamando o método atacar
+const herois = [
+    new heroi("Eduardo", 45, "ninja"),
+];
+
+herois.forEach(heroi => heroi.atacar());
